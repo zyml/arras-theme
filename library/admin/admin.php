0da3e6b43a90f8ee97472d9eb4a9893af360bca9
Melvin Lee <zy@zy.sg>
2009-06-02 14:44:31 +0000

    zy: [1.3] added description on where to place custom background images

--- library/admin/admin.php
+++ library/admin/admin.php
@@ -3,10 +3,7 @@ function arras_addmenu() {
 	$options_page = add_menu_page( __('Arras Theme', 'arras'), __('Arras Theme', 'arras'), 8, 'arras-options', 'arras_admin', get_template_directory_uri() . '/images/icon.png');
 	add_submenu_page( 'arras-options', __('Arras Theme', 'arras'), __('Theme Options', 'arras'), 8, 'arras-options', 'arras_admin' );
 	add_submenu_page( 'arras-options', __('Arras Theme', 'arras'), __('Quick Guide', 'arras'), 8, 'arras-guide', 'arras_guide' );
-	
-	//$options_page = add_theme_page( __('Options', 'arras'), __('Theme Options', 'arras'), 'edit_themes', 'arras-options', 'arras_admin' );
-	//$usage_page = add_theme_page( __('Usage Guide', 'arras'), __('Quick Guide', 'arras'), 'edit_themes', 'arras-guide', 'arras_guide' );
-	
+
 	add_action('admin_print_scripts-'. $options_page, 'arras_admin_scripts');
 	add_action('admin_print_styles-'. $options_page, 'arras_admin_styles');
 }
