68c1cb8de21e54a91b81f3b97e66739323ceda44
Melvin Lee <zy@zy.sg>
2010-03-06 17:45:37 +0000

    zy: [1.4] updated donation link

--- library/admin/templates/options_page.php
+++ library/admin/templates/options_page.php
@@ -81,19 +81,7 @@
 	<div class="postbox">
 		<h3><span><?php _e('Donate!', 'arras') ?></span></h3>
 		<p><?php _e('If you are using Arras Theme and like it, donate to the author!', 'arras') ?></p>
-		<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-			<input type="hidden" name="cmd" value="_xclick"><input type="hidden" name="business" value="zy@zy.sg" />
-			<input type="hidden" name="item_name" value="Arras Theme Donation" />
-			<input type="hidden" name="item_number" value="arrastheme_2010_donation" />
-			<input type="hidden" name="no_shipping" value="1" />
-			<input type="hidden" name="return" value="http://www.arrastheme.com/" />
-			<input type="hidden" name="cancel_return" value="http://www.arrastheme.com/" />
-			<input type="hidden" name="currency_code" value="USD" />
-			<input type="hidden" name="tax" value="0" />
-			<input type="hidden" name="bn" value="PP-DonationsBF" />
-			<input class="button-primary" type="submit" name="submit" value="<?php _e('Donate using PayPal', 'arras') ?>" />
-			<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-		</form>
+		<a class="button-primary" href="http://www.arrastheme.com/donate/"><?php _e('Donate using PayPal', 'arras') ?></a>
 	</div>
 	
 </div>
