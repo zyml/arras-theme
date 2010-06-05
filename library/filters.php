cf0286ab6bc9eedb958ae6a243f8b08f3a4bb6b2
Melvin Lee <zy@zy.sg>
2010-06-05 14:09:41 +0000

    zy: Removed single post custom fields function.

--- library/filters.php
+++ library/filters.php
@@ -70,32 +70,6 @@ function arras_postfooter() {
 }
 
 /**
- * Called to display post meta information in single posts (review scores, product information, etc.)
- * @since 1.2.2
- */
-function arras_postmeta($content) {
-	global $post;
-
-	$postmeta = '';
-	
-	$custom_fields_list = arras_parse_single_custom_fields();
-	
-	if ($custom_fields_list) {
-		foreach($custom_fields_list as $field_id => $field_name) {
-			if ( $field_value = get_post_meta($post->ID, $field_id, true) ) {
-				$postmeta .= '<div class="single-post-meta clearfix">';
-				$postmeta .= '<span class="single-post-meta-field single-post-meta-' . $field_id . '">' . $field_name . '</span>';
-				$postmeta .= '<span class="single-post-meta-value single-post-meta-' . $field_id . '-value">' . $field_value . '</span>';
-				$postmeta .= '</div>';
-			}
-		}
-	}
-	
-	if ( arras_get_option('single_meta_pos') == 'bottom' ) return $postmeta . $content;
-	else return $content . $postmeta;
-}
-
-/**
  * Displays when the specified post/archive requested by the user is not found.
  * @since	1.2.2
  */
