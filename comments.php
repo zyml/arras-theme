6fd4bc6601a07d99b4f5c611e21385328a5e1996
Melvin Lee <zy@zy.sg>
2010-07-19 16:15:09 +0000

    zy: Removed duplicate #respond DIV tag.

--- comments.php
+++ comments.php
@@ -39,25 +39,25 @@ if ( have_comments() ) : ?>
 	<h4 class="module-title"><?php _e('Comments Closed', 'arras') ?></h4>
 	<p class="nocomments"><?php _e('Comments are closed. You will not be able to post a comment in this post.', 'arras') ?></p>
 <?php endif; else: ?>
-	<div id="respond">
-		<?php
-		$req = get_option('require_name_email');
-		$aria_req = ( $req ? ' aria-required="true"' : '' );
-		$commenter = wp_get_current_commenter();
-		
-		comment_form( 
-			array(
-				'fields' => array(
-					'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Name' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
-					'<input id="author" class="required" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></p>',
-					'email' => '<p class="comment-form-email"><label for="email">' . __( 'Email' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
-					'<input id="email" class="required email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' /></p>',
-					'url'    => '<p class="comment-form-url"><label for="url">' . __( 'Website' ) . '</label>' .
-					'<input id="url" class="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /></p>'
-				),
-				'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true" class="required"></textarea></p>'
-			) 
-		); 
-		?>
-	</div><!-- #respond -->
+
+	<?php
+	$req = get_option('require_name_email');
+	$aria_req = ( $req ? ' aria-required="true"' : '' );
+	$commenter = wp_get_current_commenter();
+	
+	comment_form( 
+		array(
+			'fields' => array(
+				'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Name' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
+				'<input id="author" class="required" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></p>',
+				'email' => '<p class="comment-form-email"><label for="email">' . __( 'Email' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
+				'<input id="email" class="required email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' /></p>',
+				'url'    => '<p class="comment-form-url"><label for="url">' . __( 'Website' ) . '</label>' .
+				'<input id="url" class="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /></p>'
+			),
+			'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true" class="required"></textarea></p>'
+		) 
+	); 
+	?>
+	
 <?php endif ?>
