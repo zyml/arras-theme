d82884226f46904fcd4fbe9fb7e2e2654a8bc393
Melvin Lee <zy@zy.sg>
2010-08-10 15:35:50 +0000

    zy: Fixed page titles not getting the <h1> tags.

--- library/filters.php
+++ library/filters.php
@@ -13,7 +13,7 @@ function arras_postheader() {
 	
 	$postheader = '';
 	
-	if ( is_single() ) {
+	if ( is_single() || is_page() ) {
 	
 		if ( is_attachment() ) {
 			$postheader .= '<h1 class="entry-title">' . get_the_title() . ' [<a href="' . get_permalink($post->post_parent) . '" rev="attachment">' . get_the_title($post->post_parent) . '</a>]</h1>';
