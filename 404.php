bc66ac1f7e1e280db82997b3b1276ce2739ea6f4
Melvin Lee <zy@zy.sg>
2012-08-07 02:33:36 +0800

    fixed strings that caused validation errors with PO files.

--- 404.php
+++ 404.php
@@ -6,9 +6,10 @@
 <div class="single-post">
 	<h1 class="entry-title"><?php _e('Error 404 - Not Found', 'arras') ?></h1>
 	<div class="entry-content clearfix">
-		<?php _e('<p><strong>We\'re very sorry, but that page doesn\'t exist or has been moved.</strong><br />
-		Please make sure you have the right URL.</p>
-		<p>If you still can\'t find what you\'re looking for, try using the search form below.</p>', 'arras') ?>
+		<p><strong><?php _e( "We're very sorry, but that page doesn't exist or has been moved.", 'arras' ) ?></strong><br />
+		<?php _e( 'Please make sure you have the right URL.', 'arras' ) ?>
+		</p>
+		<p><?php _e( "If you still can't find what you're looking for, try using the search form below.", 'arras' ) ?></p>
 		<?php get_search_form(); ?>
 	</div>
 </div>
